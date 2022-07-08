e77c5e0b23035163a82b9bd72e07d0dbc9848f50
Alexander Globa <grobych@gmail.com>
2022-07-08 13:35:56 +0300

    fix onActivityCreated deprecated

--- app/src/main/java/com/globa/cocktails/fragments/CocktailFragment.kt
+++ app/src/main/java/com/globa/cocktails/fragments/CocktailFragment.kt
@@ -23,10 +23,10 @@ class CocktailFragment : Fragment() {
         return inflater.inflate(R.layout.cocktail_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CocktailViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[CocktailViewModel::class.java]
+
     }
 
 }
--- app/src/main/java/com/globa/cocktails/fragments/CocktailListFragment.kt
+++ app/src/main/java/com/globa/cocktails/fragments/CocktailListFragment.kt
@@ -23,10 +23,9 @@ class CocktailListFragment : Fragment() {
         return inflater.inflate(R.layout.cocktail_list_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CocktailListViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[CocktailListViewModel::class.java]
     }
 
 }
