e6bd44f77bb0560d427c24e653f69be8bce938d7
a.globa <a.globa@intetics.com>
2023-03-30 22:51:59 +0300

    update CocktailListScreen second version

--- app/src/main/java/com/globa/cocktails/ui/CocktailListScreen.kt
+++ app/src/main/java/com/globa/cocktails/ui/CocktailListScreen.kt
@@ -1,13 +1,26 @@
 package com.globa.cocktails.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.globa.cocktails.R
 import com.globa.cocktails.datalayer.models.Cocktail
@@ -30,7 +43,7 @@ fun CocktailListScreen(viewModel: CocktailListViewModel) {
                     items(uiState.value.cocktailList) {
                         CocktailListItem(cocktail = it)
                     }
-                }
+                },
             )
         }
     }
@@ -38,14 +51,47 @@ fun CocktailListScreen(viewModel: CocktailListViewModel) {
 
 @Composable
 fun CocktailListItem(cocktail: Cocktail) {
-    Row {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 10.dp)
+    ) {
         AsyncImage(
             model = cocktail.imageURL,
             contentDescription = cocktail.drinkName,
-            placeholder = painterResource(id = R.drawable.loading_img)
+            placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.broken_image),
+            modifier = Modifier.size(130.dp)
         )
-        Text(text = cocktail.drinkName)
+        Column {
+            Text(
+                text = cocktail.drinkName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
+                fontSize = 16.sp
+            )
+            TagField(cocktail.ingredients)
+        }
+
     }
+}
+
+@Composable
+fun TagField(list: List<String>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        Modifier.fillMaxWidth().height(100.dp),
+        content = {
+        items(list) {
+            Button(
+                onClick = { /*TODO*/ },
+                Modifier.size(40.dp,40.dp).padding(4.dp)
+            ) {
+                Text(text = it, fontSize = 10.sp, modifier = Modifier.fillMaxSize())
+            }
+        }
+    })
 }
 
 @Preview
