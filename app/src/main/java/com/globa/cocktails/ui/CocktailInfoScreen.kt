294e54222c08ca2c16eacaf5b0f4389eabe1169e
a.globa <a.globa@intetics.com>
2023-03-31 22:50:58 +0300

    add compose for CocktailInfoScreen

---
+++ app/src/main/java/com/globa/cocktails/ui/CocktailInfoScreen.kt
@@ -0,0 +1,40 @@
+package com.globa.cocktails.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.globa.cocktails.ui.viewmodels.CocktailViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.globa.cocktails.datalayer.models.Cocktail
+
+@Composable
+fun CocktailInfoScreen(
+    viewModel: CocktailViewModel = viewModel()
+) {
+
+    val cocktail by viewModel.cocktail.collectAsState()
+
+    CocktailInfo(cocktail = cocktail)
+}
+
+@Composable
+fun CocktailInfo(cocktail: Cocktail) {
+    Row(Modifier.fillMaxWidth()) {
+        AsyncImage(
+            model = cocktail.imageURL, 
+            contentDescription = cocktail.drinkName,
+            modifier = Modifier.size(width = 180.dp, height = 270.dp)
+        )
+        Text(text = cocktail.drinkName)
+    }
+    Row(Modifier.fillMaxWidth()) {
+        Text(text = cocktail.instructions)
+    }
+}
--- app/src/main/java/com/globa/cocktails/ui/viewmodels/CocktailViewModel.kt
+++ app/src/main/java/com/globa/cocktails/ui/viewmodels/CocktailViewModel.kt
@@ -2,35 +2,47 @@ package com.globa.cocktails.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.globa.cocktails.datalayer.models.Cocktail
+import com.globa.cocktails.datalayer.repository.CocktailRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class CocktailViewModel @AssistedInject constructor(
-    @Assisted("cocktail") cocktail: Cocktail
+    @Assisted("cocktailId") cocktailId: String,
+    repository: CocktailRepository
 ) : ViewModel() {
 
-    private val _cocktail = MutableStateFlow(cocktail)
+    private val _cocktail = MutableStateFlow(Cocktail())
     val cocktail = _cocktail.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            val cocktail = repository.getCocktails().find { it.id == cocktailId }
+            cocktail?.let { _cocktail.value = it } // TODO
+        }
+    }
 }
 
 class CocktailViewModelFactory @AssistedInject constructor(
-    @Assisted("cocktail") private val cocktail: Cocktail
+    @Assisted("cocktailId") private val cocktailId: String,
+    private val repository: CocktailRepository
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CocktailViewModel::class.java)) {
-            return CocktailViewModel(cocktail) as T
+            return CocktailViewModel(cocktailId, repository) as T
         }
         throw IllegalArgumentException("Unable to construct viewmodel")
     }
 
     @AssistedFactory
     interface Factory {
-        fun create(@Assisted("cocktail") cocktail: Cocktail): CocktailViewModelFactory
+        fun create(@Assisted("cocktailId") cocktailId: String): CocktailViewModelFactory
     }
 }
