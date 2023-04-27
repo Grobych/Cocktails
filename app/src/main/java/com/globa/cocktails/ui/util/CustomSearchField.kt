621057eb787adaee883f189299b8c153e51d621c
a.globa <a.globa@intetics.com>
2023-04-27 20:11:20 +0300

    add search icon in searchfield

--- app/src/main/java/com/globa/cocktails/ui/util/CustomSearchField.kt
+++ app/src/main/java/com/globa/cocktails/ui/util/CustomSearchField.kt
@@ -4,14 +4,17 @@ import android.content.res.Configuration
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,10 +22,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.globa.cocktails.R
 import com.globa.cocktails.ui.theme.AppTheme
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -34,49 +41,60 @@ fun CustomSearchField(
     onTextChanged: (TextFieldValue) -> Unit,
     onTagClicked: (String) -> Unit
 ) {
-    FlowRow(
+    Box(
         modifier = modifier
+            .width(246.dp)
+            .height(30.dp)
             .clip(shape = MaterialTheme.shapes.large)
             .background(
                 color = MaterialTheme.colorScheme.surfaceVariant,
                 shape = MaterialTheme.shapes.medium
-            )
-            .height(30.dp)
-            .requiredWidth(246.dp)
-            .horizontalScroll(state = ScrollState(0)),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        tags.forEach {
-            TagButton(
-                text = it,
-                modifier = Modifier.padding(start = 3.dp),
-                onClickAction = {onTagClicked(it)}
-            )
-        }
-        BasicTextField(
-            value = text,
-            onValueChange = onTextChanged,
-            textStyle = MaterialTheme.typography.bodyMedium.plus(
-                TextStyle(color = MaterialTheme.colorScheme.onSurfaceVariant)
             ),
-            singleLine = true,
-        ) { innerTextField ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(start = 10.dp),
-                contentAlignment = Alignment.CenterStart
-            ) {
-                if (text.text.isEmpty() && tags.isEmpty()) {
-                    Text(
-//                        text = stringResource(R.string.search_placeholder),
-                        text = "Search for cocktails",
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                } else innerTextField()
+        contentAlignment = Alignment.CenterStart
+    ) {
+        FlowRow(
+            modifier = Modifier
+                .horizontalScroll(state = ScrollState(0)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            tags.forEach {
+                TagButton(
+                    text = it,
+                    modifier = Modifier.padding(start = 3.dp),
+                    onClickAction = {onTagClicked(it)}
+                )
             }
-
+            BasicTextField(
+                value = text,
+                onValueChange = onTextChanged,
+                textStyle = MaterialTheme.typography.bodyMedium.plus(
+                    TextStyle(color = MaterialTheme.colorScheme.onSurfaceVariant)
+                ),
+                singleLine = true,
+            ) { innerTextField ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 10.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (text.text.isEmpty() && tags.isEmpty()) {
+                        Text(
+                            text = stringResource(R.string.search_placeholder),
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    } else innerTextField()
+                }
+            }
+        }
+        if (text.text.isEmpty()) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_search),
+                contentDescription = "Search",
+                modifier = Modifier.align(Alignment.CenterEnd).padding(end = 10.dp)
+            )
         }
     }
 }
