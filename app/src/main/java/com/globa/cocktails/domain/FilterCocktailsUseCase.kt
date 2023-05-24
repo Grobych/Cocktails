dd28f46df5363d04dc234de0511a867a7f9d251d
a.globa <a.globa@intetics.com>
2023-05-24 11:48:07 +0300

    update FilterCocktailUseCase

--- app/src/main/java/com/globa/cocktails/domain/FilterCocktailsUseCase.kt
+++ app/src/main/java/com/globa/cocktails/domain/FilterCocktailsUseCase.kt
@@ -2,16 +2,18 @@ package com.globa.cocktails.domain
 
 import com.globa.cocktails.datalayer.models.Cocktail
 import com.globa.cocktails.datalayer.models.createTagLine
+import javax.inject.Inject
 
-object FilterCocktailsUseCase {
-    fun List<Cocktail>.filterByTags(tags: List<String>) : List<Cocktail> =
-        this.filter { it.contains(tags) }
-}
+class FilterCocktailsUseCase @Inject constructor(){
+    operator fun invoke(cocktails: List<Cocktail>, tags: List<String>): List<Cocktail> =
+        cocktails.filter { it.contains(tags) }
 
-fun Cocktail.contains(tags: List<String>): Boolean {
-    val cocktailTagsLine = this.createTagLine()
-    tags.forEach { tag ->
-        if (!cocktailTagsLine.contains(tag, ignoreCase = true)) return false
+    fun Cocktail.contains(tags: List<String>): Boolean {
+        val cocktailTagsLine = this.createTagLine()
+        tags.forEach { tag ->
+            if (!cocktailTagsLine.contains(tag, ignoreCase = true)) return false
+        }
+        return true
     }
-    return true
 }
+
