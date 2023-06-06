6fe67cc29b5d6dde4e3919169cb9c96d594ce2b8
a.globa <a.globa@intetics.com>
2023-06-06 10:41:25 +0300

    remove unused class

--- app/src/main/java/com/globa/cocktails/datalayer/models/CocktailFilter.kt
+++
@@ -1,8 +0,0 @@
-package com.globa.cocktails.datalayer.models
-
-data class CocktailFilter(
-    var name : String = "",
-    var type : String = "All",
-    var hasAlcohol : String = "All"
-//    val ingredients : List<String> = listOf()
-)
