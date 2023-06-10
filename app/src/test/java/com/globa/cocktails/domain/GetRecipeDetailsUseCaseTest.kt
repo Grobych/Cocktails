f920337f1dc8253038c0b13f814ac6c4b1678d6d
a.globa <a.globa@intetics.com>
2023-06-10 14:33:28 +0300

    add GetRecipeDetailsUseCaseTest

---
+++ app/src/test/java/com/globa/cocktails/domain/GetRecipeDetailsUseCaseTest.kt
@@ -0,0 +1,37 @@
+package com.globa.cocktails.domain
+
+import com.globa.cocktails.datalayer.repository.CocktailRepository
+import com.globa.cocktails.domain.getrecipesdetails.GetRecipeDetailsUseCase
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class GetRecipeDetailsUseCaseTest {
+    private val repository = mockk<CocktailRepository>()
+    private val getCocktailByIdUseCase = GetCocktailByIdUseCase(repository)
+
+    private val getRecipeDetailsUseCase = GetRecipeDetailsUseCase(getCocktailByIdUseCase)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun getRecipeDetailUseCase() = runTest {
+        val id = 1
+        val testCocktail = Cocktail(
+            id = id,
+            drinkName = "Test",
+            isFavorite = true,
+            instructions = "Some instructions"
+        )
+        coEvery { repository.getCocktail(id) } returns flowOf(testCocktail)
+        val result = getRecipeDetailsUseCase(id).first()
+
+        assert(result.id == testCocktail.id)
+        assert(result.isFavorite == testCocktail.isFavorite)
+        assert(result.instructions == testCocktail.instructions)
+        assert(result.name == testCocktail.drinkName)
+    }
+}
