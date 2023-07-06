50a9374406ccb93bae44c302ac959235969fc849
a.globa <a.globa@intetics.com>
2023-07-06 16:21:09 +0300

    update loading animation

--- ui/src/main/java/com/globa/cocktails/ui/util/LoadingAnimation.kt
+++ ui/src/main/java/com/globa/cocktails/ui/util/LoadingAnimation.kt
@@ -1,74 +1,78 @@
 package com.globa.cocktails.ui.util
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.LinearOutSlowInEasing
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.InfiniteRepeatableSpec
 import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
+import com.globa.cocktails.ui.R
+import com.globa.cocktails.ui.theme.AppTheme
 
 @Composable
 fun LoadingAnimation(
     modifier: Modifier = Modifier,
-    circleSize: Dp = 25.dp,
-    circleColor: Color = MaterialTheme.colorScheme.primary,
-    spaceBetween: Dp = 10.dp,
-    travelDistance: Dp = 20.dp
+    backgroundColor: Color = MaterialTheme.colorScheme.primaryContainer
 ) {
-    val circles = listOf(
-        remember { Animatable(initialValue = 0f) },
-        remember { Animatable(initialValue = 0f) },
-        remember { Animatable(initialValue = 0f) }
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val degree = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 30f,
+        animationSpec = InfiniteRepeatableSpec(
+            animation = tween(
+                durationMillis = 300,
+                delayMillis = 500,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
     )
 
-    circles.forEachIndexed { index, animatable ->
-        LaunchedEffect(key1 = animatable) {
-            delay(index * 100L)
-            animatable.animateTo(
-                targetValue = 1f,
-                animationSpec = infiniteRepeatable(
-                    animation = keyframes {
-                        durationMillis = 1200
-                        0.0f at 0 with LinearOutSlowInEasing
-                        1.0f at 300 with LinearOutSlowInEasing
-                        0.0f at 600 with LinearOutSlowInEasing
-                        0.0f at 1200 with LinearOutSlowInEasing
-                    },
-                    repeatMode = RepeatMode.Restart
-                )
-            )
-        }
+    Box(
+        modifier = modifier
+            .background(color = backgroundColor)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        val id = if (isSystemInDarkTheme()) R.drawable.ic_shaker_dark else R.drawable.ic_shacker_light
+        Image(
+            modifier = Modifier
+                .size(75.dp,150.dp)
+                .rotate(degree.value)
+            ,
+            imageVector = ImageVector.vectorResource(id = id),
+            contentDescription = stringResource(id = R.string.loading_string)
+        )
     }
+}
 
-    val circleValues = circles.map { it.value }
-    val distance = with(LocalDensity.current) { travelDistance.toPx() }
-    val lastCircle = circleValues.size - 1
-
-    Row(modifier = modifier) {
-        circleValues.forEachIndexed { index, value ->
-            Box(modifier = Modifier
-                .size(circleSize)
-                .graphicsLayer { translationY = -value * distance }
-                .background(color = circleColor, shape = CircleShape)
-            )
-            if (index != lastCircle) Spacer(modifier = Modifier.width(spaceBetween))
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun LoadingAnimationPreview() {
+    AppTheme {
+        Surface {
+            LoadingAnimation()
         }
     }
 }
